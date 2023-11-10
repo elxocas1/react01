9e9c92efc65d896659dba699cdc2ada8f5c1ffb1
elxocas1 <fabianpachao20@gmail.com>
2023-11-10 11:45:13 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           <code>I.E.S.P."Honorio Delgado Espinoza  -----  Turno: Mañana"</code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Fabian Fabrizio Pachao Apaza
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
